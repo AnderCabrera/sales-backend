19ce7a9c99127bbb4bd37c2d4a333d3a75ff0c97
acabrera-2022508 <acabrera-2022508@kinal.edu.gt>
2024-02-23 02:35:54 -0600

    adding admin user default

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import connection from './db/mongo.js';
+import { hashPassword } from './helpers/bcrypt.js';
 import 'dotenv/config';
 
+// Models
+import User from './models/user.model.js';
+
+// Routes
 import userRoutes from './routes/user.routes.js';
 
 const app = express();
@@ -21,8 +26,27 @@ app.use(morgan('dev'));
 app.use('/user', userRoutes);
 
 // Connection
-connection().then(() => {
-  app.listen(PORT, () => {
-    console.log(`server listening on Port ${PORT}`);
+connection()
+  .then(async () => {
+    // Create admin user
+    const admin = await User({
+      name: 'Admin',
+      lastName: 'Admin',
+      username: 'admin',
+      password: await hashPassword('admin'),
+      role: 'ADMIN',
+      cart: [],
+      purchases: [],
+    });
+
+    const users = await User.find({});
+
+    if (users.length === 0) {
+      await admin.save();
+    }
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server listening on Port ${PORT}`);
+    });
   });
-});
