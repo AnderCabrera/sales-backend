ba1a015cbed16f2040d05326762d22e780ef73f0
acabrera-2022508 <acabrera-2022508@kinal.edu.gt>
2024-03-07 01:57:41 -0600

    refactoring product and user routes

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,87 @@
+import Product from '../models/product.model.js';
+import { hashPassword, comparePassword } from '../helpers/bcrypt.js';
+import { generateToken } from '../helpers/jwt.js';
+import { isLoggedIn } from '../middlewares/isLoggedIn.js';
+import { isAdmin } from '../middlewares/isAdmin.js';
+
+export const addProduct = async (req, res) => {
+  try {
+    let { name, description, price, stock, category } = req.body;
+
+    const products = await Product.find({});
+    let productAlreadyExists = products.some(
+      (product) => product.name === name,
+    );
+
+    if (productAlreadyExists) {
+      let productToUpdate = await Product.findOne({ name });
+
+      await Product.findOneAndUpdate(
+        { name },
+        {
+          stock: productToUpdate.stock + 1,
+        },
+        { new: true },
+      );
+
+      return res.send({
+        message: 'The product exists added +1 to this product',
+      });
+    }
+
+    const newProduct = new Product({
+      name,
+      description,
+      price,
+      stock,
+      category,
+    });
+
+    await newProduct.save();
+
+    return res.status(200).send('Product added succesfully');
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  try {
+    const { productName } = req.params;
+    const { name, description, price, stock, category } = req.body;
+
+    const productToUpdate = await Product.findOne({ name: productName });
+
+    if (!productToUpdate) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    await Product.findOneAndUpdate(
+      { name: productName },
+      { name, description, price, stock, category },
+      { new: true },
+    );
+
+    return res.json({ message: 'Product updated' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const { productName } = req.params;
+
+    const productToDelete = await Product.findOne({ name: productName });
+
+    if (!productToDelete) {
+      return res.status(404).json({ message: 'Course not found' });
+    }
+
+    await Product.findOneAndDelete({ name: productName });
+
+    return res.json({ message: 'Course deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,124 +1,21 @@
 import express from 'express';
-import User from '../models/user.model.js';
-import { hashPassword, comparePassword } from '../helpers/bcrypt.js';
-import { generateToken } from '../helpers/jwt.js';
 import { isLoggedIn } from '../middlewares/isLoggedIn.js';
+
+// Controllers
+import {
+  login,
+  register,
+  profile,
+  update,
+  deleteUser,
+} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.post('/login', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-
-    const isPasswordValid = await comparePassword(password, user.password);
-
-    if (isPasswordValid) {
-      let loggedUser = {
-        uid: user._id,
-        username: username,
-        role: user.role,
-      };
-
-      const token = await generateToken(loggedUser);
-
-      return res.send({
-        message: 'Logged in',
-        token,
-      });
-    }
-
-    return res.status(401).json({ message: 'Invalid credentials' });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.post('/register', async (req, res) => {
-  try {
-    const { name, lastName, username, password } = req.body;
-
-    const user = new User({
-      name,
-      lastName,
-      username,
-      password: await hashPassword(password),
-      role: 'CLIENT',
-      cart: [],
-      purchases: [],
-    });
-
-    const users = await User.find({});
-
-    let userAlreadyExists = users.some((user) => user.username === username);
-
-    if (userAlreadyExists) {
-      return res
-        .status(400)
-        .json({ message: 'Username already exists, use another' });
-    }
-
-    await user.save();
-
-    return res.json({ message: 'User registered' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.get('/profile', isLoggedIn, async (req, res) => {
-  try {
-    const user = await User.findById({ _id: req.user._id }).select('-_id');
-
-    return res.json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.put('/update', isLoggedIn, async (req, res) => {
-  try {
-    const { name, lastName, username, password } = req.body;
-
-    const users = await User.find({});
-    let userAlreadyExists = users.some((user) => user.username === username);
-
-    if (userAlreadyExists) {
-      return res
-        .status(400)
-        .json({ message: 'Username already in use, use another' });
-    }
-
-    await User.findOneAndUpdate(
-      { _id: req.user._id },
-      {
-        name,
-        lastName,
-        // username,
-        // password: await hashPassword(password),
-      },
-      { new: true },
-    );
-
-    return res.json({ message: 'Profile updated' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.delete('/delete', isLoggedIn, async (req, res) => {
-  try {
-    await User.findOneAndDelete({ _id: req.user._id });
-
-    return res.json({ message: 'Profile deleted' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.post('/login', login);
+router.post('/register', register);
+router.get('/profile', isLoggedIn, profile);
+router.put('/update', isLoggedIn, update);
+router.delete('/delete', isLoggedIn, deleteUser);
 
 export default router;
